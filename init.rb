5be6444d87e9ea03bc73f609ce10e796c37fc5ed
g301244 <g301244@gmail.com>
2016-12-24 00:47:12 +0900

    余分なhrタグを出力しないように修正

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_custom_field_views do
   name 'Redmine Custom Field Views plugin'
   author 'g301244'
   description 'This is the plugin which customizes view in a custom field for Redmine.'
-  version '0.1.0'
+  version '0.1.1'
   url 'https://github.com/g301244/redmine_custom_field_views'
   author_url 'https://github.com/g301244'
 end
