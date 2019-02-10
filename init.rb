b17c849fdc06f09b3d6636c8f7daad411da2bcd0
sawai <sawai.t@jp.fujitsu.com>
2019-02-11 01:57:55 +0900

    Redmine 4.0対応

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_custom_field_views do
   name 'Redmine Custom Field Views plugin'
   author 'g301244'
   description 'This is the plugin which customizes view in a custom field for Redmine.'
-  version '0.1.1'
+  version '0.2.0'
   url 'https://github.com/g301244/redmine_custom_field_views'
   author_url 'https://github.com/g301244'
 end
--- lib/redmine_custom_field_views/patches/redmine_custom_field_views_issues_helper_patch.rb
+++ lib/redmine_custom_field_views/patches/redmine_custom_field_views_issues_helper_patch.rb
@@ -1,49 +1,45 @@
 require_dependency 'issues_helper'
 
 module RedmineCustomFieldViews
-  module Patches
-    module RedmineCustomFieldViewsIssuesHelperPatch
-      def self.included(base)
-        base.send(:include, RedmineCustomFieldViewsInstanceMethods)
-        base.class_eval do
-          unloadable
-          alias_method_chain :render_custom_fields_rows, :redmine_custom_field_views
-        end
-      end
+  module IssuesHelperPatch
 
-      module RedmineCustomFieldViewsInstanceMethods
-        def render_custom_fields_rows_with_redmine_custom_field_views(issue)
-          s = ""
-          columns_by_types = allocation_columns(issue.custom_field_values)
-          columns_by_types.each do |columns_by_type|
-            columns_by_type.each do |key, values|
-              if key == "2"
-                half = (values.size / 2.0).ceil
-                s << issue_fields_rows do |rows|
-                  values.each_with_index do |value, i|
-                    css = "cf_#{value.custom_field.id}"
-                    m = (i < half ? :left : :right)
-                    rows.send m, custom_field_name_tag(value.custom_field), show_value(value), :class => css
-                  end
-                end
-              elsif key == "1"
-                issue_fields_rows do |rows|
-                  values.each do |value|
-                    css = "cf_#{value.custom_field.id}"
-                    rows.send :left, custom_field_name_tag(value.custom_field), show_value(value), :class => css
-                  end
-                  s << content_tag('div', rows.left.reduce(&:+).gsub('<div class="value">', '<div class="value" style="overflow-x:auto;">').gsub('<p>', '<p style="margin:0">').html_safe, class: 'splitcontent')
-                end
+    # Under Redmine 3.3.x
+    def render_custom_fields_rows(issue)
+      s = ""
+      allocation_columns(issue.custom_field_values).each do |columns_by_type|
+        columns_by_type.each do |key, values|
+          if key == "2"
+            half = (values.size / 2.0).ceil
+            s << issue_fields_rows do |rows|
+              values.each_with_index do |value, idx|
+                css = "cf_#{value.custom_field.id}"
+                m = (idx < half ? :left : :right)
+                rows.send m, custom_field_name_tag(value.custom_field), show_value(value), class: css
               end
             end
+          elsif key == "1"
+            issue_fields_rows do |rows|
+              values.each do |value|
+                css = "cf_#{value.custom_field.id}"
+                rows.send :left, custom_field_name_tag(value.custom_field), show_value(value), class: css
+              end
+              s << content_tag('div', rows.left.reduce(&:+).gsub('<div class="value">', '<div class="value" style="overflow-x:auto;">').gsub('<p>', '<p style="margin:0">').html_safe, class: 'splitcontent')
+            end
           end
-          s.gsub(content_tag('div', '', class: 'splitcontentleft'), '').html_safe
         end
       end
+      s.gsub(content_tag('div', '', class: 'splitcontentleft'), '').html_safe
+    end
+
+    # Over Redmine 3.4.x
+    def render_half_width_custom_fields_rows(issue)
+     render_custom_fields_rows(issue)
+    end
+
+    # Over Redmine 3.4.x
+    def render_full_width_custom_fields_rows(issue)
     end
   end
 end
 
-unless IssuesHelper.included_modules.include?(RedmineCustomFieldViews::Patches::RedmineCustomFieldViewsIssuesHelperPatch)
-  IssuesHelper.send(:include, RedmineCustomFieldViews::Patches::RedmineCustomFieldViewsIssuesHelperPatch)
-end
+IssuesHelper.prepend(RedmineCustomFieldViews::IssuesHelperPatch)
